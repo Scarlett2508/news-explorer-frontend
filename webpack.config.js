747fa1349b40ba5a413f2aba203a112bae5b58b2
Alevtina <fedinaalevtina@gmail.com>
2020-07-09 19:30:06 +0300

    some plugin change

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,8 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackMd5Hash = require('webpack-md5-hash');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = {
   entry: { main: './src/index.js' },
@@ -17,19 +19,46 @@ module.exports = {
     },
     {
       test: /\.css$/,
-      use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
+      use: [
+        {
+          loader: MiniCssExtractPlugin.loader,
+          options: { publicPath: '../' },
+        },
+        'css-loader', 'postcss-loader',
+      ],
+    },
+    {
+      test: /\.(png|jpg|gif|ico|svg)$/,
+      use: [
+        'file-loader?name=./images/[name].[ext]',
+        {
+          loader: 'image-webpack-loader',
+          options: {},
+        },
+      ],
+    },
+    {
+      test: /\.(eot|ttf|woff|woff2)$/,
+      loader: 'file-loader?name=./vendor/[name].[ext]',
     },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      inject: false,
       template: './src/index.html',
-      filename: 'index.html',
     }),
     new MiniCssExtractPlugin({
-      filename: 'style.[contenthash].css',
+      filename: '[name]/[name].[contenthash].css',
     }),
+    new CleanWebpackPlugin(),
     new WebpackMd5Hash(),
+    new OptimizeCssAssetsWebpackPlugin({
+      assetNameRegExp: /\.css$/g,
+      cssProcessor: require('cssnano'),
+      cssProcessorPluginOptions: {
+        preset: ['default'],
+      },
+      canPrint: true,
+    }),
   ],
 };
