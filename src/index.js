7ff6fb3876d265d49bb9b120a7f46d410a93408c
Alevtina <fedinaalevtina@gmail.com>
2020-07-18 16:50:05 +0300

    started js part

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import './page/index.css';
+
+import { MainApi } from './js/api/MainApi';
+import { NewsApi } from './js/api/NewsApi';
+
+import config from './js/constants/config';
+
+const {
+  MAINAPI_URL, NEWSAPI_KEY, NEWSAPI_URL, PAGE_SIZE, NEWSAPI_DAYS,
+} = config;
+
+const mainApi = new MainApi(MAINAPI_URL);
+const newsApi = new NewsApi (NEWSAPI_URL, NEWSAPI_KEY, PAGE_SIZE, NEWSAPI_DAYS);
---
+++ src/js/constants/config.js
@@ -0,0 +1,9 @@
+const config = {
+  MAINAPI_URL: 'https://newsforsave.tk/',
+  NEWSAPI_KEY: 'cb24e49a16d84dcab3b2b76fb7714be8',
+  NEWSAPI_URL: 'https://newsapi.org',
+  PAGE_SIZE: '100',
+  NEWSAPI_DAYS: 7,
+};
+
+export default config;
